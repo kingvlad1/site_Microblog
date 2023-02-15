9883b99362f26d52365811d4df453b1a42511435
Vladyslav <99832659+kingvlad1@users.noreply.github.com>
2023-02-15 13:26:19 +0100

    Всі імпорти та модулі проєкту

---
+++ imports.py
@@ -0,0 +1,10 @@
+from flask import render_template, flash, redirect, url_for
+from app import app
+from app.forms import LoginForm
+import datetime
+from app import db
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo
+
+#Flask, Flask-wtf
