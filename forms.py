00d13beadbb4cb7fa615a58b16c5602e90349742
Vladyslav <99832659+kingvlad1@users.noreply.github.com>
2023-02-26 23:27:00 +0100

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,3 +1,4 @@
+#Wt-Forms = WTF!!
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired, Email, EqualTo
