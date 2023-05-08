c85cc6650d68f89dcf88cdfa6ade461a13d776a1
Vladyslav <99832659+kingvlad1@users.noreply.github.com>
2023-05-08 18:47:23 +0300

    Update forms.py

--- forms.py
+++ forms.py
@@ -2,7 +2,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired, Email, EqualTo
-
+#WTF?
 
 class LoginForm(FlaskForm):
     username = StringField("Ім'я", validators=[DataRequired()])
